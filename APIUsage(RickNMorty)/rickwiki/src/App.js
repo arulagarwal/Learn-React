ce51152784cc4520e57b8ec98e59a8c164ce03ee
Arul Agarwal <arulagarwal@gmail.com>
2021-03-02 00:51:34 +0530

    updates

--- APIUsage(RickNMorty)/rickwiki/src/App.js
+++ APIUsage(RickNMorty)/rickwiki/src/App.js
@@ -1,12 +1,44 @@
 import './App.css';
+import axios from 'axios';
 import React, {Component} from 'react';
 
+ 
+
 class App extends Component {
-  render(){
+  state = {
+    characters: []
+  }
+  componentDidMount() {
+    axios.get('https://rickandmortyapi.com/api/character/')
+    .then(res=>this.setState({characters:res.data}))
+  }
+  render( ){
+    
+    const {results = []} = this.state.characters;
+    console.log('here',results);
     return(
+      
       <div className ="App">
-       <header className = "App-header"> Hello </header> 
-        </div>
+        <head>
+        <title>Rick and Morty Character Wiki</title>
+        <link rel="icon" href="/favicon.ico" />
+        </head>
+        <ul className="grid">
+  {results.map(result => {
+    const { id, name, image } = result;
+    const base = window.location.origin + '/';
+    console.log(base);
+    return (
+      <li key={id} className="card">
+        <a href= {`${base}${id}`}>
+        <img src={image} alt={`${name} Thumbnail`} />          
+        <h3>{ name }</h3>
+        </a>
+      </li>
+    )
+  })}
+</ul>
+      </div>
     );
   }
 }
